9809cc56cc469653d9e10ba3d649abd367b817b0
白貓貓-kei <545078012@qq.com>
2026-06-08 17:00:44 +0800

    Revert "Fix Astro build and TypeScript checks"

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,16 @@ import icon from 'astro-icon'
 import svelte from '@astrojs/svelte'
 
 export default defineConfig({
-  site: 'https://alaster-t34.github.io',
   integrations: [
     icon(),
     svelte(),
   ],
   vite: {
+    esbuild: {
+      loader: 'tsx',
+      include: /src\/.*\.tsx?$/,
+      exclude: [],
+    },
     server: {
       hmr: {
         port: 24678,
